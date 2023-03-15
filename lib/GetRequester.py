4d452478f25a3558b5d072e9fe8d2c68d95bbacf
Bianca Aspin <bianca@mj94.net>
2023-03-14 17:15:11 -0700

    Complete lab.

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,20 @@
 import requests
 import json
 
+url =  'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        people = json.loads(self.get_response_body())
+        people_list = [person for person in people]
+        return people_list
+
+# import ipdb; ipdb.set_trace()
